b829944488054dd6a56e25befb4dd389b47bd6d8
Kaeltis <info@kaeltis.de>
2017-01-15 14:02:40 +0100

    changes to seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,7 +13,56 @@ class UsersTableSeeder extends Seeder
     {
         $faker = Faker\Factory::create('de_DE');
 
-        foreach (range(1, 30) as $index) {
+        DB::table('users')->insert([
+            'name' => $faker->lastName,
+            'firstname' => $faker->firstName,
+            'email' => 'test@test.test',
+            'password' => bcrypt('gnDeEjNcfYb4ADEWCgCwy24p'),
+            'street' => $faker->streetName,
+            'housenumber' => $faker->buildingNumber,
+            'zipcode' => $faker->postcode,
+            'city' => $faker->city,
+            'phone' => $faker->phoneNumber,
+            'birthdate' => $faker->dateTimeThisCentury->format('Y-m-d'),
+            'sharetoken' => str_random(32),
+            'role' => \App\User::EMPLOYEE,
+        ]);
+
+        foreach (range(1, 5) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker->lastName,
+                'firstname' => $faker->firstName,
+                'email' => $faker->email,
+                'password' => bcrypt($faker->password),
+                'street' => $faker->streetName,
+                'housenumber' => $faker->buildingNumber,
+                'zipcode' => $faker->postcode,
+                'city' => $faker->city,
+                'phone' => $faker->phoneNumber,
+                'birthdate' => $faker->dateTimeThisCentury->format('Y-m-d'),
+                'sharetoken' => str_random(32),
+                'role' => \App\User::EMPLOYEE,
+            ]);
+        }
+
+        foreach (range(1, 5) as $index) {
+            DB::table('users')->insert([
+                'name' => $faker->lastName,
+                'firstname' => $faker->firstName,
+                'email' => $faker->email,
+                'password' => bcrypt($faker->password),
+                'street' => $faker->streetName,
+                'housenumber' => $faker->buildingNumber,
+                'zipcode' => $faker->postcode,
+                'city' => $faker->city,
+                'phone' => $faker->phoneNumber,
+                'birthdate' => $faker->dateTimeThisCentury->format('Y-m-d'),
+                'sharetoken' => str_random(32),
+                'role' => \App\User::PROFESSOR,
+            ]);
+        }
+
+        foreach (range(1, 20) as $index) {
             DB::table('users')->insert([
                 'name' => $faker->lastName,
                 'firstname' => $faker->firstName,
